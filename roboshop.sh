4027eeca66a1af1022a4b5555088d014a14d8d26
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-05-25 22:27:27 +0530

    Creating instance via CLI

--- roboshop.sh
+++ roboshop.sh
@@ -6,15 +6,19 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z05939662N1222ETTVGZT" # replace with your ZONE ID
 DOMAIN_NAME="roboshop.fun" # replace with your domain
 
-for instance in ${INSTANCES[@]}
-#for instance in $@
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-089cd1efcc22c1659 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+                RECORD_NAME="$DOMAIN_NAME"
+
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+                RECORD_NAME="$DOMAIN_NAME"
+
     fi
     echo "$instance IP address: $IP"
 
